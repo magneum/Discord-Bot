6afb32d472a1304c6f1351cf97dc27c215d1d7c6
magneum <krakinzkon@gmail.com>
2023-06-24 11:57:06 -0400

    updated

--- database/models/userBans.js
+++
@@ -1,9 +0,0 @@
-const mango = require("mongoose");
-const path = require("path");
-
-module.exports = mango.model(
-  path.basename(__filename, path.extname(__filename)),
-  new mango.Schema({
-    User: String,
-  })
-);
--- events/client/raw.js
+++
@@ -1,5 +0,0 @@
-const discord = require("discord.js");
-
-module.exports = async (client, d) => {
-  client.player.updateVoiceState(d);
-};
