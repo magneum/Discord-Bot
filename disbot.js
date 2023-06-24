061f96f9d83ff2c3965596d8496dd490ffac0a5e
magneum <krakinzkon@gmail.com>
2023-06-24 11:22:48 -0400

    updated

--- disbot.js
+++ disbot.js
@@ -1,251 +1,235 @@
-const pgs = require("./config/pgs.js");
+const webhook = require("../database/json/webhooks.json");
 const discord = require("discord.js");
 require("dotenv").config("./.env");
 const chalk = require("chalk");
+
+const webHooksArray = [
+  "startLogs",
+  "shardLogs",
+  "errorLogs",
+  "dmLogs",
+  "voiceLogs",
+  "serverLogs",
+  "serverLogs2",
+  "commandLogs",
+  "consoleLogs",
+  "warnLogs",
+  "voiceErrorLogs",
+  "creditLogs",
+  "evalLogs",
+  "interactionLogs",
+];
+
+if (process.env.WEBHOOK_ID && process.env.WEBHOOK_TOKEN) {
+  for (const webHook of webHooksArray) {
+    switch (webHook) {
+      case "startLogs":
+      case "shardLogs":
+      case "errorLogs":
+      case "dmLogs":
+      case "voiceLogs":
+      case "serverLogs":
+      case "serverLogs2":
+      case "commandLogs":
+      case "consoleLogs":
+      case "warnLogs":
+      case "voiceErrorLogs":
+      case "creditLogs":
+      case "evalLogs":
+      case "interactionLogs":
+        webhook[webHook].id = process.env.WEBHOOK_ID;
+        webhook[webHook].token = process.env.WEBHOOK_TOKEN;
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+const startLogs = new discord.WebhookClient({
+  token: webhook.startLogs.token,
+  id: webhook.startLogs.id,
+});
+
+const shardLogs = new discord.WebhookClient({
+  token: webhook.shardLogs.token,
+  id: webhook.shardLogs.id,
+});
+
+const manager = new discord.ShardingManager("./brain.js", {
+  token: process.env.DISCORD_TOKEN,
+  totalShards: "auto",
+  respawn: true,
+});
+
+if (process.env.TOPGG_TOKEN) {
+  const { AutoPoster } = require("topgg-autoposter");
+  AutoPoster(process.env.TOPGG_TOKEN, manager);
+}
+
 console.clear();
-
-const disbot = async () => {
-  const tableNames = [
-    "startLogs",
-    "shardLogs",
-    "errorLogs",
-    "dmLogs",
-    "voiceLogs",
-    "serverLogs",
-    "serverLogs2",
-    "commandLogs",
-    "consoleLogs",
-    "warnLogs",
-    "voiceErrorLogs",
-    "creditLogs",
-    "evalLogs",
-    "interactionLogs",
-  ];
-
-  const createTablesIfNotExist = async () => {
-    try {
-      for (const tableName of tableNames) {
-        const query = `
-CREATE TABLE IF NOT EXISTS ${tableName} (
-id SERIAL PRIMARY KEY,
-data JSONB
+console.log(
+  chalk.blue.bold("System") +
+    chalk.white(">>") +
+    chalk.green("Starting up") +
+    chalk.white("...")
 );
-`;
-        await pgs.DATABASE.query(query);
-      }
-    } catch (error) {
-      console.error(chalk.bold.red("❌ Error creating tables:"), error);
-    }
-  };
+console.log("\u001b[0m");
+console.log(chalk.red("© MagneumDev | 2021 - " + new Date().getFullYear()));
+console.log(chalk.red("All rights reserved"));
+console.log("\u001b[0m");
+console.log("\u001b[0m");
+console.log(
+  chalk.blue.bold("System") +
+    chalk.white(">>") +
+    chalk.red("Version " + require(`${process.cwd()}/package.json`).version) +
+    chalk.green("loaded")
+);
+console.log("\u001b[0m");
 
-  try {
-    await pgs.DATABASE.authenticate();
-    console.log(chalk.bold.green("✅ Connected to PostgreSQL!"));
-    await createTablesIfNotExist();
-  } catch (error) {
-    console.error(chalk.bold.red("❌ Failed to connect to PostgreSQL:"), error);
-    return;
-  }
-
-  const webhook = {};
-
-  for (const tableName of tableNames) {
-    webhook[tableName] = tableName;
-  }
-
-  const sendLog = async (tableName, logData) => {
-    const query = {
-      text: `INSERT INTO ${tableName} (data) VALUES ($1)`,
-      values: [JSON.stringify(logData)],
-    };
-
-    try {
-      await pgs.DATABASE.query(query.text, query.values);
-    } catch (error) {
-      console.error(
-        chalk.bold.red(`❌ Error inserting log into ${tableName}:`),
-        error
-      );
-    }
-  };
-
-  const startLogs = {
-    tableName: webhook.startLogs,
-  };
-
-  const shardLogs = {
-    tableName: webhook.shardLogs,
-  };
-
-  const consoleLogs = {
-    tableName: webhook.consoleLogs,
-  };
-
-  const warnLogs = {
-    tableName: webhook.warnLogs,
-  };
-
-  const manager = new discord.ShardingManager("./brain.js", {
-    token: process.env.DISCORD_TOKEN,
-    totalShards: "auto",
-    respawn: true,
+manager.on("shardCreate", async (shard) => {
+  await startLogs.send({
+    username: "Bot Logs",
+    embeds: [
+      new discord.EmbedBuilder()
+        .setTitle("🆙・Launching shard")
+        .setDescription("A shard has just been launched")
+        .setFields([
+          {
+            name: "🆔┆ID",
+            value: `${shard.id + 1}/${manager.totalShards}`,
+            inline: true,
+          },
+          {
+            name: "📃┆State",
+            value: "Starting up...",
+            inline: true,
+          },
+        ])
+        .setColor("#5865F2"),
+    ],
   });
 
-  switch (process.env.TOPGG_TOKEN) {
-    case true:
-      const { AutoPoster } = require("topgg-autoposter");
-      AutoPoster(process.env.TOPGG_TOKEN, manager);
-      break;
-    default:
-      break;
-  }
-
   console.log(
-    chalk.bold.blue("System") +
+    chalk.blue.bold("System") +
       chalk.white(">>") +
-      chalk.green("✨ Starting up") +
+      chalk.green("Starting") +
+      chalk.red("Shard #" + (shard.id + 1)) +
       chalk.white("...")
   );
-  console.log(
-    chalk.bold.blue("© MagneumDev | 2021 - " + new Date().getFullYear())
-  );
-  console.log(chalk.bold.blue("All rights reserved"));
   console.log("\u001b[0m");
-  console.log("\u001b[0m");
-  console.log(
-    chalk.bold.blue("System") +
-      chalk.white(">>") +
-      chalk.red(
-        "📦 Version " + require(`${process.cwd()}/package.json`).version
-      ) +
-      chalk.green(" loaded")
-  );
 
-  manager.on("shardCreate", async (shard) => {
-    await sendLog(startLogs.tableName, {
+  shard.on("death", async (process) => {
+    await shardLogs.send({
       username: "Bot Logs",
       embeds: [
-        {
-          title: "🆙 Launching shard",
-          description: "A shard has just been launched",
-          fields: [
+        new discord.EmbedBuilder()
+          .setTitle(
+            "🚨・Closing shard " +
+              (shard.id + 1) +
+              "/" +
+              manager.totalShards +
+              " unexpectedly"
+          )
+          .setFields([
             {
-              name: "🆔 ID",
+              name: "🆔┆ID",
               value: `${shard.id + 1}/${manager.totalShards}`,
-              inline: true,
             },
-            {
-              name: "📃 State",
-              value: "Starting up...",
-              inline: true,
-            },
-          ],
-          color: "#5865F2",
-        },
+          ])
+          .setColor("#5865F2"),
       ],
     });
 
-    console.log(
-      chalk.bold.blue("System") +
-        chalk.white(">>") +
-        chalk.green("✨ Starting") +
-        chalk.red(" Shard #" + (shard.id + 1)) +
-        chalk.white("...")
-    );
-
-    shard.on("death", async (process) => {
-      await sendLog(shardLogs.tableName, {
+    if (process.exitCode === null) {
+      await shardLogs.send({
         username: "Bot Logs",
         embeds: [
-          {
-            title: `🚨 Closing shard ${shard.id + 1}/${
-              manager.totalShards
-            } unexpectedly`,
-            fields: [
+          new discord.EmbedBuilder()
+            .setTitle(
+              "🚨・Shard " +
+                (shard.id + 1) +
+                "/" +
+                manager.totalShards +
+                " exited with NULL error code!"
+            )
+            .setFields([
               {
-                name: "🆔 ID",
-                value: `${shard.id + 1}/${manager.totalShards}`,
+                name: "PID",
+                value: "`" + process.pid + "`",
               },
-            ],
-            color: "#5865F2",
-          },
-        ],
-      });
-
-      switch (process.exitCode === null) {
-        case true:
-          await sendLog(shardLogs.tableName, {
-            username: "Bot Logs",
-            embeds: [
               {
-                title: `🚨 Shard ${shard.id + 1}/${
-                  manager.totalShards
-                } exited with NULL error code!`,
-                fields: [
-                  {
-                    name: "PID",
-                    value: "`" + process.pid + "`",
-                  },
-                  {
-                    name: "Exit code",
-                    value: "`" + process.exitCode + "`",
-                  },
-                ],
-                color: "#5865F2",
+                name: "Exit code",
+                value: "`" + process.exitCode + "`",
               },
-            ],
-          });
-          break;
-        default:
-          break;
-      }
-    });
-
-    shard.on("shardDisconnect", async (event) => {
-      await sendLog(shardLogs.tableName, {
-        username: "Bot Logs",
-        embeds: [
-          {
-            title: `🚨 Shard ${shard.id + 1}/${
-              manager.totalShards
-            } disconnected`,
-            description: "Dumping socket close event...",
-            color: "#5865F2",
-          },
+            ])
+            .setColor("#5865F2"),
         ],
       });
-    });
+    }
+  });
 
-    shard.on("shardReconnecting", async () => {
-      await sendLog(shardLogs.tableName, {
-        username: "Bot Logs",
-        embeds: [
-          {
-            title: `🚨 Reconnecting shard ${shard.id + 1}/${
-              manager.totalShards
-            }`,
-            color: "#5865F2",
-          },
-        ],
-      });
+  shard.on("shardDisconnect", async (event) => {
+    await shardLogs.send({
+      username: "Bot Logs",
+      embeds: [
+        new discord.EmbedBuilder()
+          .setTitle(
+            "🚨・Shard " +
+              (shard.id + 1) +
+              "/" +
+              manager.totalShards +
+              " disconnected"
+          )
+          .setDescription("Dumping socket close event...")
+          .setColor("#5865F2"),
+      ],
     });
   });
 
-  manager.spawn();
-
-  process.on("unhandledRejection", async (error) => {
-    console.error(chalk.bold.red("Unhandled promise rejection:"), error);
-    if (error && error.length > 950)
-      error = error.slice(0, 950) + "... view console for details";
-    if (error.stack && error.stack.length > 950)
-      error.stack = error.stack.slice(0, 950) + "... view console for details";
-    if (!error.stack) return;
-    await sendLog(consoleLogs.tableName, {
+  shard.on("shardReconnecting", async () => {
+    await shardLogs.send({
       username: "Bot Logs",
       embeds: [
-        {
-          title: "🚨・Unhandled promise rejection",
-          fields: [
+        new discord.EmbedBuilder()
+          .setTitle(
+            "🚨・Reconnecting shard " +
+              (shard.id + 1) +
+              "/" +
+              manager.totalShards
+          )
+          .setColor("#5865F2"),
+      ],
+    });
+  });
+});
+
+manager.spawn();
+
+const consoleLogs = new discord.WebhookClient({
+  id: webhook.consoleLogs.id,
+  token: webhook.consoleLogs.token,
+});
+
+const warnLogs = new discord.WebhookClient({
+  id: webhook.warnLogs.id,
+  token: webhook.warnLogs.token,
+});
+
+process.on("unhandledRejection", async (error) => {
+  console.error("Unhandled promise rejection:", error);
+  if (error && error.length > 950)
+    error = error.slice(0, 950) + "... view console for details";
+  if (error.stack && error.stack.length > 950)
+    error.stack = error.stack.slice(0, 950) + "... view console for details";
+  if (!error.stack) return;
+  await consoleLogs
+    .send({
+      username: "Bot Logs",
+      embeds: [
+        new discord.EmbedBuilder()
+          .setTitle("🚨・Unhandled promise rejection")
+          .addFields([
             {
               name: "Error",
               value: error ? discord.codeBlock(error) : "No error",
@@ -256,31 +240,31 @@ data JSONB
                 ? discord.codeBlock(error.stack)
                 : "No stack error",
             },
-          ],
-        },
+          ]),
       ],
+    })
+    .catch(() => {
+      console.log("Error sending unhandled promise rejection to webhook");
+      console.log(error);
     });
-  });
+});
 
-  process.on("warning", async (warn) => {
-    console.warn(chalk.bold.yellow("Warning:"), warn);
-    await sendLog(warnLogs.tableName, {
+process.on("warning", async (warn) => {
+  console.warn("Warning:", warn);
+  await warnLogs
+    .send({
       username: "Bot Logs",
       embeds: [
-        {
-          title: "🚨・New warning found",
-          fields: [
-            {
-              name: "Warn",
-              value: "```" + warn + "```",
-            },
-          ],
-        },
+        new discord.EmbedBuilder().setTitle("🚨・New warning found").addFields([
+          {
+            name: "Warn",
+            value: "```" + warn + "```",
+          },
+        ]),
       ],
+    })
+    .catch(() => {
+      console.log("Error sending warning to webhook");
+      console.log(warn);
     });
-  });
-};
-
-disbot().catch((error) => {
-  console.error(chalk.bold.red("❌ An error occurred during startup:"), error);
 });
